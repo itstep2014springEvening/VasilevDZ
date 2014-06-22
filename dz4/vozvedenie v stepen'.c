2990fa468b1aa0efb62b4fcc3eb8770fd5974f0f
maixi <www.vacek95@mail.ru>
2014-06-22 14:24:04 +0300

    dz nomer 4

---
+++ dz4/vozvedenie v stepen'.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, a;
+    printf ("Vvedite n \n");
+    scanf ("%d", &n);
+    for (int b=1; n>0; b*=a)
+    {
+        printf ("Vvedide chislo ");
+        scanf ("%d", &a);
+        n-=1;
+    }
+    return 0;
+}
+
