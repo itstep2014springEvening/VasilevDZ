4ba0a35ada215004d982abd29ee419dd162459c2
maixi <www.vacek95@mail.ru>
2014-06-24 11:16:32 +0300

    fixed

--- dz3/fixed.c
+++ dz3/fixed.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 int main()
-{   int a,b;
-printf("vvedite 1 chislo");
-scanf("%d",&a);
-printf("vvedite 2 chislo ");
-scanf("%d",&b);
-printf("\n summa = ");
+{
+    int a,b;
+    printf("vvedite 1 chislo");
+    scanf("%d",&a);
+    printf("vvedite 2 chislo ");
+    scanf("%d",&b);
+    printf("\n summa = ");
     printf("%d + %d=%d",a,b,a+b);
     printf("\n raznost' = ");
     printf("%d - %d=%d",a,b,a-b);
