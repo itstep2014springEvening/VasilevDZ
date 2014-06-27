8cb83976288c66828a7a84b0e4f21434d29e86b3
maixi <www.vacek95@mail.ru>
2014-06-27 16:11:52 +0300

    cos

--- dz11/cos/main.c
+++ dz11/cos/main.c
@@ -5,25 +5,25 @@ int main()
 
 {
     double x,a;
-    printf("vvedite x , dlya vishitivaniya sin(x)\n");
+    printf("vvedite x , dlya vishitivaniya cos(x)\n");
     scanf("%lf",&x);
     printf("x=%f\n",x);
-    double sum=x,dobavka=x,eps=0.001;
+    double sum=1,dobavka=1,eps=0.001;
     int i=1;
     int j=1;
     int k=-1;
     while(fabs(dobavka)>eps)
     {
-        dobavka=dobavka*(x*x)/((i+1)*(i+2));
+        dobavka=dobavka*(x*x)/((i+1)*(i));
         sum=sum + pow(k,j)*dobavka;
         ++i;
         ++i;
         ++j;
 
     }
-    printf("sin=%f\n",sum);
-    a=sin(x);
-    printf("sin funkcii =%f\n",a);
+    printf("cos=%f\n",sum);
+    a=cos(x);
+    printf("cos funkcii =%f\n",a);
     printf("raznost' = %f\n",fabs(a-sum));
 
 }
